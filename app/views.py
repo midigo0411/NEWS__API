fe7eb48cb36ab447ecfa2708423399e0b8274f0b
midigo0411 <midigo041@gmail.com>
2021-06-03 16:29:54 +0300

    my commit

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app import app
+from .request import get_movies
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular = popular_movies)
